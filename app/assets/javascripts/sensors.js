488320cb213ab9a93b7a18bfa14c0a4eadc2296f
Andreas Ruppen <andreas.ruppen@gmail.com>
2014-07-01 20:09:49 +0200

    Implemented a Smart Light Bulb Arduino Sketch

--- app/assets/javascripts/sensors.js
+++ app/assets/javascripts/sensors.js
@@ -7,17 +7,27 @@ $('select').on('change', function() {
     console.log( this.value );
 });
 
-function updateThermistor(slider, value, comselector)
+function updateThermistor(slider, value)
 {
     //console.log(this)
-    console.log("hi2")
-    console.log(slider)
-    $('#log1').text(value)
+    //$('#log1').text(value)
     var temp = $('#thermistor1').val();
     var hum = $('#humidity1').val();
     var com = $("#serialPortSelector1 option:selected").val()
     console.log(com);
     var jsonText={'temp':temp, 'hum':hum, 'com':com};
-    $.ajax({url:'', data:jsonText, dataType:'json', cache:false, type:'POST', success:function(data){$('#'+slider+'text').text(data['value'])}})
+    $.ajax({url:'', data:jsonText, dataType:'json', cache:false, type:'POST', success:function(data){$('#log1').text(value)}});
+    $('#'+slider+'text').text(value)
+}
+
+function updateLightBulb(slider, value)
+{
+    var light = $('#lightsensor').val();
+    var onoff = $('#lightswitch').prop('checked')
+    var com = $("#serialPortSelector2 option:selected").val()
+    console.log(com);
+    var jsonText={'light':light, 'switch':onoff, 'com':com};
+    $.ajax({url:'', data:jsonText, dataType:'json', cache:false, type:'POST', success:function(data){$('#log3').text(value)}});
+    $('#'+slider+'text').text(value);
 }
 
